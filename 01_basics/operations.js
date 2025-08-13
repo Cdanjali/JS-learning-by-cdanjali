1064e2a0b8f21f3b445dbc383396042986e8b9af
anjali verma <Anjaliverma8924@gmail.com>
2025-08-13 09:24:48 +0000

    operations

---
+++ 01_basics/operations.js
@@ -0,0 +1,18 @@
+// *********************OPERATIONS**********************
+/*
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+console/log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2%2);
+
+*/
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "3");
