2909b759a38e1aae23b87f38e19ac95661547b7c
anjali verma <Anjaliverma8924@gmail.com>
2025-08-13 07:30:18 +0000

    Add variable.js file

---
+++ 01_basics/variable.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "anjali@google.com"
+var accountPassword = "122112"
+accountCity = "indore"
+
+accountEmail = "ashye@google.com"
+accountPassword = "1212121212"
+accountCity = "pune"
+
+console.log(accountId)
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+/*
+prefer not to use var because os issue in block scope and functional  scope
+
+*/
