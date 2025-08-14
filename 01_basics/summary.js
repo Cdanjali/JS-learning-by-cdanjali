8830863dc63f029e06e87fd92eb636ef11f1e44a
anjali verma <Anjaliverma8924@gmail.com>
2025-08-14 09:28:27 +0000

    summary of datatypes

---
+++ 01_basics/summary.js
@@ -0,0 +1,18 @@
+
+
+let myYoutubeName = "anjaliVermajii"
+
+let anotherName = myYoutubeName
+anotherName = "Chai Or Code"
+
+console.log(anotherName)
+console.log(myYoutubeName)
+
+let userOne = {
+    email: "user@gmail.com",
+    upi: "user@ybl"
+}
+let userTwo = userOne;
+userTwo.email = "anjali@gmmail.com";
+console.log(userOne.email);
+console.log(userTwo.email);
