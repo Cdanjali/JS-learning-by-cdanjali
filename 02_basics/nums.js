e16e87d62fbb935cd066cf45a87409f5fe27c1df
anjali verma <Anjaliverma8924@gmail.com>
2025-08-18 11:19:27 +0000

    some basic about numbers and maths  in js

---
+++ 02_basics/nums.js
@@ -0,0 +1,29 @@
+/*
+const score = 400
+console.log(score);
+const balance = new Number(1000);
+console.log(balance)
+
+console.log(balance.toString())
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
+
+const otherNumber = 123.56987474
+console.log(otherNumber.toPrecision(4))
+console.log(otherNumber.toPrecision(3))
+console.log(otherNumber.toPrecision(2))
+
+const hundreads = 100000000
+console.log(hundreads.toLocaleString('en-IN'));
+*/
+
+
+/*********************maths*****************/
+
+console.log(Math);
+console.log(Math.abs(-96));
+console.log(Math.round(465.2));
+
+console.log(Math.round(465.5));
+console.log(Math.min(4, 5, 9, 8, 5, 3, 2, 1));
+console.log(Math.max(4, 5, 9, 8, 5, 3, 2, 1));
