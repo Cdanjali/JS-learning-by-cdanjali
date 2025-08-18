fa3f38203f5075547e2b5c45fcdba042f8d9012b
anjali verma <Anjaliverma8924@gmail.com>
2025-08-18 10:30:19 +0000

    String in javascripts

---
+++ 02_basics/Stings.js
@@ -0,0 +1,19 @@
+const name = "Lucky";
+const repoCount = 50
+console.log(name + repoCount + "value");
+
+console.log(`HEllo my name is ${name} and i have ${repoCount} reppos`);
+
+const gameNAme = new String('Luckyyyyychhhchc')
+console.log(gameNAme[0]);
+console.log(gameNAme.__proto__);
+console.log(gameNAme.length);
+console.log(gameNAme.charAt('6'));
+console.log(gameNAme.indexOf('c'))
+const newString = gameNAme.substring(0, 5);
+console.log(newString);
+const anothernewString = gameNAme.slice(-3, 3);
+console.log(anothernewString);
+const newStringOne = "   lucky       ";
+console.log(newStringOne);
+console.log(newStringOne.trim());
