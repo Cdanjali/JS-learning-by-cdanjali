0b44c321e72f9dee1483216ba84afc75f8ddd32c
anjali verma <Anjaliverma8924@gmail.com>
2025-08-22 08:47:35 +0000

    functions in js

---
+++ functions/function2.js
@@ -0,0 +1,38 @@
+const { use } = require("react");
+
+
+function one() {
+    const usersName = "LUcky1227Verma"
+
+    function two() {
+        const website = "you tube channel"
+        console.log(usersName);
+    }
+    //console.log(website);
+
+    //    two();
+}
+
+//one();
+
+if (true) {
+    const usersName = "Lucky"
+    if (usersName === "Lucky") {
+        const website = "youTube"
+        console.log(usersName + website);
+
+    }
+    //console.log(website)
+}
+//console.log(usersName)
+
+function adding(value) {
+    return value + 1;
+}
+console.log(adding(5));
+
+const addTwo = function (value) {
+    return value + 2;
+}
+
+console.log(addTwo(5));
