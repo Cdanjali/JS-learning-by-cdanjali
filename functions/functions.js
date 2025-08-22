48bda1ed5bfccf66f6fd78c2b9b9aece267d648c
anjali verma <Anjaliverma8924@gmail.com>
2025-08-22 08:25:41 +0000

    functions in js

---
+++ functions/functions.js
@@ -0,0 +1,20 @@
+
+
+function sayMyName() {
+    console.log("L");
+    console.log("U");
+    console.log("C");
+    console.log("K");
+    console.log("Y");
+}
+//sayMyName();
+function add(num1, num2) {
+    //console.log(num1 + num2);
+}
+add(5, 5);
+
+function loginUser(usersName) {
+    return `${usersName} just logged in`
+}
+console.log(loginUser("lucky"));
+
