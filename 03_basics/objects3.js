b908a79e1f59a281ba0b502cc7726a379d5d8855
anjali verma <Anjaliverma8924@gmail.com>
2025-08-22 07:43:53 +0000

    some objects formats in js

---
+++ 03_basics/objects3.js
@@ -0,0 +1,11 @@
+const course = {
+    courseName: " javascroit in hindi",
+    price: "999",
+    courseId: "js1234",
+    courseInstructoe: "john dee",
+}
+const { courseInstructoe } = course
+console.log(courseInstructoe);
+
+const { courseInstructoe: instructor } = course
+console.log(instructor);
